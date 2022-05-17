d3109ea9d6dd5199818829554dd6b5fa7d020918
Guga <contato.gustavobarbosa@outlook.com>
2022-05-17 15:15:57 -0300

    Preparando revisão de código

--- middlewares/userMiddlewares.js
+++
@@ -1,9 +0,0 @@
-import { transactionSchema } from "../schemas/userSchemas.js";
-import validateSchema from "../schemas/validateSchema.js";
-
-export async function validateTransaction(req, res, next) {
-  if (!validateSchema(req, transactionSchema)) {
-    return res.sendStatus(422);
-  }
-  next();
-}
